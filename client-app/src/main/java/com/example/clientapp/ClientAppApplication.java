95cb9b14e07f8d9f694f15274228a33faaff49b6
Marcus Da Coregio <marcusdacoregio@gmail.com>
2023-08-15 15:50:21 -0300

    Add native hints

--- client-app/src/main/java/com/example/clientapp/ClientAppApplication.java
+++ client-app/src/main/java/com/example/clientapp/ClientAppApplication.java
@@ -1,13 +1,29 @@
 package com.example.clientapp;
 
+import java.util.ArrayList;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(ClientAppApplication.MyHints.class)
 public class ClientAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientAppApplication.class, args);
+    }
+
+    static class MyHints implements RuntimeHintsRegistrar {
+
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.reflection().registerType(ArrayList.class, MemberCategory.INVOKE_DECLARED_METHODS);
+        }
+
     }
 
 }
